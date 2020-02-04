65dcd31fca530dfe9fc4c321bcd7652671f50afb
Roshan-AS <sayyalas@ucsd.edu>
2020-02-03 19:06:19 -0800

    added comments

--- detect_breathing.m
+++ detect_breathing.m
@@ -1,8 +1,10 @@
 for i = 1:3
     load(['data_',num2str(i),'.mat']);
-    t_window = wal_T(end)-wal_T(1);
-    fft_wal = abs(fft(wal_e));
-    fft_band = abs(fft(wal_e_band));
+    % take fft of the data to figure out the frequency of heart rates and
+    % breath rates. What will be the sampling frequnecy in Frequnecy
+    % domain? (Think about the Time windowing you are doing)
+    
+    %
     breath_rate_walabot = 
     heart_rate_walabot =
     breath_rate_band = 
