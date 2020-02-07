fa01f6fc079de136bb27eb79c54dc659983f3887
Roshan-AS <sayyalas@ucsd.edu>
2020-02-06 20:09:55 -0800

    updated skeleton

--- detect_breathing.m
+++ detect_breathing.m
@@ -1,14 +1,15 @@
 for i = 1:3
     load(['data_',num2str(i),'.mat']);
+    % wal_e is the data measured by Walabot
+    % wal_e_band is the data measured by Band
+    % wal_T is the time stamps in seconds recorded by walabot.
     % take fft of the data to figure out the frequency of heart rates and
     % breath rates. What will be the sampling frequnecy in Frequnecy
     % domain? (Think about the Time windowing you are doing)
     
     %
-    breath_rate_walabot = 
-    heart_rate_walabot =
-    breath_rate_band = 
-    heart_rate_band =
+    breath_rate_walabot(i) = 
+
 end
 
 
